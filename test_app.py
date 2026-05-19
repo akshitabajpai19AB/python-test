8cfc4bff181e6f36c25ccb4093e0b96b848c7acd
akshitabajpai19AB <akshita19.ab@gmail.com>
2026-05-19 21:32:40 +0530

    add

--- test_app.py
+++ test_app.py
@@ -1,8 +1,11 @@
 #this is a test app
 from app import add
 
+
 def test_add():
-    assert add(2, 3) == 5
+    result = add(2, 3)
+    print("Result of add(2, 3):", result)
+
 
 def bad_format():
   return 1
